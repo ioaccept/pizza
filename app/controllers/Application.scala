0ab07a6dfc3e8569e786d218fd175d810f74c0c5
Oliver Braun <ob@obraun.net>
2014-11-10 18:42:58 +0100

    Ein Benutzer kann sich registrieren.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,33 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models.User
 
 object Application extends Controller {
 
+  val userForm = Form(
+    mapping(
+      "Name" -> text)(User.apply)(User.unapply))
+
+  def addUser = Action { implicit request =>
+    userForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest(views.html.index(formWithErrors))
+      },
+      userData => {
+        val newUser = models.User(userData.name)
+        Redirect(routes.Application.showUser(newUser.name)).flashing("success" -> "User saved!")
+      })
+  }
+
+  def showUser(username : String) = Action {
+    Ok(views.html.user(username))
+  }
+  
   def index = Action {
-    Ok(views.html.index("Pizza-Flitza"))
+    Ok(views.html.index(userForm))
   }
 
 }
