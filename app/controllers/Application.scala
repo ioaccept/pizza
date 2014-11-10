95d8fa386efce0b4cdaa2a351c79f260574c0b9f
Oliver Braun <ob@obraun.net>
2014-11-10 20:04:13 +0100

    User in eine H2-Datenbank. showUsers zeigt alle registrierten User an.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
+import anorm._
+import play.api.Play.current
 import play.api._
+import play.api.db._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
@@ -19,14 +22,31 @@ object Application extends Controller {
       },
       userData => {
         val newUser = models.User(userData.name)
+        DB.withConnection { implicit c =>
+          val id: Option[Long] =
+            SQL("insert into User(name) values ({name})").on(
+              'name -> userData.name).executeInsert()
+        }
+
         Redirect(routes.Application.showUser(newUser.name)).flashing("success" -> "User saved!")
       })
   }
 
-  def showUser(username : String) = Action {
+  def showUser(username: String) = Action {
     Ok(views.html.user(username))
   }
-  
+
+  def showUsers = Action {
+    DB.withConnection { implicit c =>
+      val selectUsers = SQL("Select name from User;")
+      // Transform the resulting Stream[Row] to a List[(String,String)]
+      val users = selectUsers().map(row =>
+        row[String]("name")).toList
+      Ok(views.html.users(users))
+    }
+
+  }
+
   def index = Action {
     Ok(views.html.index(userForm))
   }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,13 @@
+# Users schema
+ 
+# --- !Ups
+ 
+CREATE TABLE User (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+ 
+# --- !Downs
+ 
+DROP TABLE User;
